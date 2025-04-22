14277e1c0869b6337c85ceb08c1f79cb53c120e7
BringFeel <69062690+BringFeel@users.noreply.github.com>
2025-04-21 21:27:51 -0300

    Delete papaplus/negocio-papa directory

--- papaplus/negocio-papa/backend/vistas/cerrar_sesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('../controladores/controladorAutenticacion.php');
-$controladorAutenticacion = new ControladorAutenticacion();
-$controladorAutenticacion->cerrarSesion();
-header("Location: login.php");
-exit();
-?>
